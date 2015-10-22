9aba34038d6e205497cd3fe4ba3fc2fcb352717e
wesb <wesb@qualtrics.com>
2015-10-21 22:47:08 -0600

    simple error

--- app.go
+++ app.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	structs "./structs"
 	"fmt"
+	"log"
 )
 
 func main() {
 	fmt.Println("hello world")
+	e := &structs.AppError{Message: "foo message", Code: 500}
+	log.Printf("Handler error: status code: %d, message: %s",
+		e.Code, e.Message)
+
 }
